247a666afd8ed43a27ca594ac53ca380a246f4b8
jansonlwj <605233702@qq.com>
2016-03-30 17:20:40 +0800

    xiugai

---
+++ src/main/java/com/thinkgem/jeesite/weixinfront/web/WeixinIndexController.java
@@ -0,0 +1,44 @@
+/**
+ * Copyright &copy; 2012-2014 <a href="https://github.com/thinkgem/jeesite">JeeSite</a> All rights reserved.
+ */
+package com.thinkgem.jeesite.weixinfront.web;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.thinkgem.jeesite.common.web.BaseController;
+import com.thinkgem.jeesite.modules.cms.utils.WiexinSignUtil;
+import com.thinkgem.jeesite.weixin.service.WeixinService;
+
+/**
+ * 微信首页
+ * @author yaominginfo
+ *
+ */
+@Controller
+@RequestMapping(value = "${weixinPath}/weixinIndex")
+public class WeixinIndexController extends BaseController {
+
+	/**
+	 * 微信首页
+	 * @param request
+	 * @return
+	 */
+	@RequestMapping(value = "index")
+	public String index(HttpServletRequest request) {
+		
+
+		return "weixin/front/index";
+	}
+
+
+}
