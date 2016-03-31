1a301d37fb0c8fed4af34b00cf6926a39266cf5a
jansonlwj <605233702@qq.com>
2016-03-31 15:39:20 +0800

    增加微信前端

---
+++ src/main/webapp/weixin/front/js/app.js
@@ -0,0 +1,135 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+// 'starter.services' is found in services.js
+// 'starter.controllers' is found in controllers.js
+angular.module('myApp', ['ionic','myApp.controllers','myApp.services'])
+    .run(function($ionicPlatform) {
+        $ionicPlatform.ready(function() {
+            // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+            // for form inputs)
+            if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+                cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+            }
+            if (window.StatusBar) {
+                // org.apache.cordova.statusbar required
+                StatusBar.styleLightContent();
+            }
+        });
+    })
+
+    .config(function($stateProvider, $urlRouterProvider) {
+
+        // Ionic uses AngularUI Router which uses the concept of states
+        // Learn more here: https://github.com/angular-ui/ui-router
+        // Set up the various states which the app can be in.
+        // Each state's controller can be found in controllers.js
+        console.log($stateProvider)
+        $stateProvider
+            //客户订单列表页
+            .state('tab', {
+                url: '/tab',
+                // abstract:'true',
+                templateUrl: "templates/tabs.html",
+                controller:"orderController"
+            })
+            //客户订单页，目前作废
+            .state('order', {
+                url: '/order',
+                templateUrl:"templates/order.html",
+                controller:"orderController"
+               /* views:{
+                    "tab.order":{
+                        templateUrl:"templates/order.html",
+                        controller:"orderController1"
+                    }
+                }*/
+            })
+            //订单新增
+            .state('orderAdd',{
+                url:'/orderAdd',
+                templateUrl:"templates/orderAdd.html",
+                controller:"orderAddController"
+            })
+            .state('orderDetail', {
+                url: '/orderDetail/:orderID',
+                templateUrl: 'templates/orderDetail.html',
+                controller: 'orderDetailController'
+            })
+            //管理员登录
+            .state('adminLogin', {
+                url: '/adminLogin',
+                templateUrl: 'templates/adminLogin.html',
+                controller: 'adminController'
+            })
+            //管理列表
+            .state('adminList', {
+                url: '/adminList',
+                templateUrl: 'templates/adminList.html',
+                controller: 'adminController'
+            })
+            //未完成订单列表
+            .state('undoneOrder', {
+                url: '/undoneOrder',
+                templateUrl: 'templates/undoneOrder.html',
+                controller: 'orderController'
+            })
+            .state('orderProcess', {
+                url: '/orderProcess/:orderID',
+                templateUrl: 'templates/orderProcess.html',
+                controller: 'orderDetailController'
+            })
+            //已完成订单列表
+            .state('doneOrder', {
+                url: '/doneOrder',
+                templateUrl: 'templates/doneOrder.html',
+                controller: 'orderController'
+            })
+
+            //客户信息管理
+            .state('customer', {
+            url: '/customer',
+            templateUrl: 'templates/customer.html',
+            controller: 'cusController'
+            })
+            .state('myInfo', {
+                url: '/myInfo/:customerID',
+                templateUrl: 'templates/myInfo.html',
+                controller: 'cusController'
+            })
+            .state('customerDetail', {
+                url: '/customerDetail/:customerID',
+                templateUrl: 'templates/customerDetail.html',
+                controller: 'cusController'
+            })
+            //员工信息管理
+            .state('employ', {
+                url: '/employ',
+                templateUrl: 'templates/employ.html',
+                controller: 'employController'
+            })
+            .state('employDetail', {
+                url: '/employDetail/:employID',
+                templateUrl: 'templates/employDetail.html',
+                controller: 'employController'
+            })
+            //配件管理
+            .state('fitting', {
+                url: '/fitting',
+                templateUrl: 'templates/fitting.html',
+                controller: 'fittingController'
+            })
+            .state('fittingDetail', {
+                url: '/fittingDetail/:fittingID',
+                templateUrl: 'templates/fittingDetail.html',
+                controller: 'fittingController'
+            })
+
+
+
+        // if none of the above states are matched, use this as the fallback
+        $urlRouterProvider.otherwise('/tab');
+
+    });
---
+++ src/main/webapp/weixin/front/js/services.js
@@ -0,0 +1,77 @@
+angular.module('myApp.services', ['ngResource'])
+
+.factory('Order', function($rootScope,$resource,$http) {
+
+  //获取客户订单列表数据
+  var cusOrderObj=$resource('resource/order.json');
+  //获取所有未完成订单列表数据
+  var undoneOrderDObj=$resource('resource/order.json');
+  //获取所有完成订单数据
+  var doneOrderDObj=$resource('resource/order.json');
+  //获取根据订单号获取订单明细
+  var detailObj=$resource('resource/order.json');
+  //获取所有完成订单瓦片数据汇总
+  var doneOrderTotalItemObj=$resource('resource/orderTotalItems.json');
+  //获取所有完成订单配件数据汇总
+  var doneOrderTotalFitObj=$resource('resource/orderTotalFit.json');
+
+  //获取配件数据
+  var fitting=$resource('resource/fits.json');
+
+  //获取客户信息
+  var customerObj=$resource('resource/customer.json');
+
+  //获取客户信息
+  var employObj=$resource('resource/employ.json');
+  //获取配件信息
+  var fittingObj=$resource('resource/fits.json');
+
+  return {
+    //返回客户订单列表数据
+    getCusOrderList:function(){
+      return cusOrderObj.query();
+    },
+    //返回所有未完成订单列表数据
+    getUndoneOrderDList:function(){
+      return undoneOrderDObj.query();
+    },
+    //返回所有完成订单列表数据
+    getDoneOrderDList:function(){
+      return doneOrderDObj.query();
+    },
+    //返回根据订单号获取订单明细
+    getDetail:function(){
+      return detailObj.query();
+    },
+    //返回所有完成订单瓦片数据汇总
+    getDoneOrderTotalItems:function(bDate,eDate){
+      //bDate开始日期 eDate结束日期
+      return doneOrderTotalItemObj.query();
+    },
+    //返回所有完成订单配件数据汇总
+    getDoneOrderTotalFits:function(bDate,eDate){
+      //bDate开始日期 eDate结束日期
+      return doneOrderTotalFitObj.query();
+    },
+    //获得配件
+    getFitting:function(){
+      return fitting.query();
+    },
+    //删除订单
+    remove: function(order) {
+      order.splice(order.indexOf(order), 1);
+    },
+    //获得客户信息
+    getCustmer:function(){
+      return customerObj.query();
+    },
+    //获得员工列表信息
+    getEmploy:function(){
+      return employObj.query();
+    },
+    getFitting:function(){
+      return fittingObj.query();
+    },
+    //
+  };
+});
