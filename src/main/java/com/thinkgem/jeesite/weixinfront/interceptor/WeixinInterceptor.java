8d7e48fff9f597585ca970a5a7e7e280a70a051f
janson <janson@janson-pc>
2016-07-08 20:22:17 +0800

    修改菜单

--- src/main/java/com/thinkgem/jeesite/weixinfront/interceptor/WeixinInterceptor.java
+++ src/main/java/com/thinkgem/jeesite/weixinfront/interceptor/WeixinInterceptor.java
@@ -19,32 +19,43 @@ import com.thinkgem.jeesite.weixin.system.entity.WeixinUserInfo;
 
 /**
  * 微信拦截器
+ * 
  * @author yaominginfo
  *
  */
 public class WeixinInterceptor extends BaseService implements HandlerInterceptor {
-	
+
 	Logger logger = LoggerFactory.getLogger(getClass());
 
-	
 	@Override
-	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, 
-			Object handler) throws Exception {
-		
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+
 		logger.info("微信地址拦截 url : {}", request.getRequestURI());
 		logger.info("微信地址拦截 model : {}", request.getMethod());
-		
-		WeixinUserInfo weixinUserInfo= (WeixinUserInfo) request.getSession().getAttribute("weixinUserInfo");
-		if(weixinUserInfo==null){
-
+		String type = request.getParameter("type");
+		WeixinUserInfo weixinUserInfo = (WeixinUserInfo) request.getSession().getAttribute("weixinUserInfo");
+		if (weixinUserInfo == null) {
 			logger.info("还没登录，被拦截！");
-			String urlbegin = "https://open.weixin.qq.com/connect/oauth2/authorize?appid="
-					+ WeixinGlobal.getAppid()
-					+ "&redirect_uri="
-					+ Global.getSystemUrl();
-			String urlcontent=Global.getWeixinPath()+"/weixinIndex/oAuth";
-			String urlend = "&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect";
-			System.out.println(urlbegin+urlcontent+urlend);
+			String urlbegin = "";
+			String urlcontent = "";
+			String urlend = "";
+			if (type.equals("admin")) {
+				urlbegin = "https://open.weixin.qq.com/connect/oauth2/authorize?appid=" + WeixinGlobal.getAppid()
+						+ "&redirect_uri=" + Global.getSystemUrl();
+				urlcontent = Global.getWeixinPath() + "/weixinIndex/oAuthAdmin";
+				urlend = "&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect";
+			} else if (type.equals("userinfo")) {
+				urlbegin = "https://open.weixin.qq.com/connect/oauth2/authorize?appid=" + WeixinGlobal.getAppid()
+						+ "&redirect_uri=" + Global.getSystemUrl();
+				urlcontent = Global.getWeixinPath() + "/weixinIndex/oAuthUserinfo";
+				urlend = "&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect";
+			} else {
+				urlbegin = "https://open.weixin.qq.com/connect/oauth2/authorize?appid=" + WeixinGlobal.getAppid()
+						+ "&redirect_uri=" + Global.getSystemUrl();
+				urlcontent = Global.getWeixinPath() + "/weixinIndex/oAuth";
+				urlend = "&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect";
+			}
 			response.sendRedirect(urlbegin + urlcontent + urlend);
 			return false;
 		}
@@ -52,15 +63,15 @@ public class WeixinInterceptor extends BaseService implements HandlerInterceptor
 	}
 
 	@Override
-	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, 
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
 			ModelAndView modelAndView) throws Exception {
 
 	}
 
 	@Override
-	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, 
-			Object handler, Exception ex) throws Exception {
-		
+	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
+			throws Exception {
+
 	}
 
 }
--- src/main/java/com/thinkgem/jeesite/weixinfront/web/WeixinIndexController.java
+++ src/main/java/com/thinkgem/jeesite/weixinfront/web/WeixinIndexController.java
@@ -33,38 +33,42 @@ import com.thinkgem.jeesite.weixinfront.util.Util;
 @Controller
 @RequestMapping(value = "${weixinPath}/weixinIndex")
 public class WeixinIndexController extends BaseController {
-	
+
 	@Autowired
 	private WeixinUserInfoService weixinUserInfoService;
 
 	/**
 	 * 微信首页
-	 * @throws Exception 
+	 * 
+	 * @throws Exception
 	 */
 	@RequestMapping(value = "index")
-	public String index(HttpServletRequest request) throws Exception {
+	public String index(HttpServletRequest request, String type) throws Exception {
 		System.out.println("WeixinIndexController.index()");
-		
-//		String weixinUserinfoString=(String) request.getSession().getAttribute("weixinUserinfoString");
-//		weixinUserinfoString = AESSessionUtil.aesDecrypt(weixinUserinfoString);
-//		WeixinUserInfo weixinUserInfo=(WeixinUserInfo) JsonMapper.fromJsonString(weixinUserinfoString, WeixinUserInfo.class);
-		WeixinUserInfo weixinUserInfo= (WeixinUserInfo) request.getSession().getAttribute("weixinUserInfo");
-		if(weixinUserInfo!=null){
-			WeixinUserInfo oldWeixinUserInfo=weixinUserInfoService.findByOpenid(weixinUserInfo.getOpenid());
-			Date now=new Date();
-			if(oldWeixinUserInfo==null){
+
+		WeixinUserInfo weixinUserInfo = (WeixinUserInfo) request.getSession().getAttribute("weixinUserInfo");
+		if (weixinUserInfo != null) {
+			WeixinUserInfo oldWeixinUserInfo = weixinUserInfoService.findByOpenid(weixinUserInfo.getOpenid());
+			Date now = new Date();
+			if (oldWeixinUserInfo == null) {
 				weixinUserInfo.setLastLoginTime(now);
 				weixinUserInfoService.save(weixinUserInfo);
-			}else{
+			} else {
 				oldWeixinUserInfo.setLastLoginTime(now);
 				weixinUserInfoService.save(oldWeixinUserInfo);
 			}
 		}
-		return "redirect:/weixinfront/front/index.html?weixinUserinfoString=12414324234dsds";
+		if ("userinfo".equals(type)) {
+			return "redirect:/weixinfront/front/index.html#/myInfo";
+		} else if ("admin".equals(type)) {
+			return "redirect:/weixinfront/front/index.html#/admin/adminLogin";
+		} else {
+			return "redirect:/weixinfront/front/index.html";
+		}
 	}
 
 	/**
-	 * 微信授权
+	 * 微信授权首页
 	 */
 	@RequestMapping(value = "oAuth")
 	public String oAuth(HttpServletRequest request, String code) {
@@ -86,15 +90,74 @@ public class WeixinIndexController extends BaseController {
 
 				// 添加session
 				request.getSession().setAttribute("weixinUserInfo", weixinUserinfo);
-//				weixinUserinfoString=JsonMapper.toJsonString(weixinUserinfo);
-//				weixinUserinfoString = AESSessionUtil.aesEncrypt(weixinUserinfoString);
-//				request.getSession().setAttribute("weixinUserinfoString",weixinUserinfoString);
-
 				logger.error("WeixinIndexController oAuth openId-->{}", openId);
 			}
 		} catch (Exception e) {
 			logger.error("WeixinIndexController oAuth error-->{}", e.getMessage());
 		}
-		return "redirect:" + Global.getWeixinPath() + "/weixinIndex/index";
+		return "redirect:" + Global.getWeixinPath() + "/weixinIndex/index?type=index";
+	}
+
+	/**
+	 * 微信授权管理员
+	 */
+	@RequestMapping(value = "oAuthAdmin")
+	public String oAuthAdmin(HttpServletRequest request, String code) {
+		try {
+			logger.info("WeixinIndexController oAuthAdmin code-->{}", code);
+			// 用户同意授权
+			if (!"authdeny".equals(code)) {
+
+				// 获取网页授权access_token
+				WeixinOauth2Token weixinOauth2Token = AdvancedUtil.getOauth2AccessToken(WeixinGlobal.getAppid(),
+						WeixinGlobal.getAppsecret(), code);
+				// 网页授权接口访问凭证
+				String accessToken = weixinOauth2Token.getAccessToken();
+				// 用户标识
+				String openId = weixinOauth2Token.getOpenId();
+
+				// 获取用户信息
+				WeixinUserInfo weixinUserinfo = AdvancedUtil.getWeixinUserinfo(accessToken, openId);
+
+				// 添加session
+				request.getSession().setAttribute("weixinUserInfo", weixinUserinfo);
+				logger.error("WeixinIndexController oAuthAdmin openId-->{}", openId);
+			}
+		} catch (Exception e) {
+			logger.error("WeixinIndexController oAuthAdmin error-->{}", e.getMessage());
+		}
+		return "redirect:" + Global.getWeixinPath() + "/weixinIndex/index?type=admin";
+	}
+
+	/**
+	 * 微信授权用户信息
+	 */
+	@RequestMapping(value = "oAuthUserinfo")
+	public String oAuthUserinfo(HttpServletRequest request, String code) {
+		try {
+			logger.info("WeixinIndexController oAuthUserinfo code-->{}", code);
+			// 用户同意授权
+			if (!"authdeny".equals(code)) {
+
+				// 获取网页授权access_token
+				WeixinOauth2Token weixinOauth2Token = AdvancedUtil.getOauth2AccessToken(WeixinGlobal.getAppid(),
+						WeixinGlobal.getAppsecret(), code);
+				// 网页授权接口访问凭证
+				String accessToken = weixinOauth2Token.getAccessToken();
+				// 用户标识
+				String openId = weixinOauth2Token.getOpenId();
+
+				// 获取用户信息
+				WeixinUserInfo weixinUserinfo = AdvancedUtil.getWeixinUserinfo(accessToken, openId);
+
+				// 添加session
+				request.getSession().setAttribute("weixinUserInfo", weixinUserinfo);
+
+				logger.error("WeixinIndexController oAuthUserinfo openId-->{}", openId);
+			}
+		} catch (Exception e) {
+			logger.error("WeixinIndexController oAuthUserinfo error-->{}", e.getMessage());
+		}
+		return "redirect:" + Global.getWeixinPath() + "/weixinIndex/index?type=userinfo";
 	}
 }
